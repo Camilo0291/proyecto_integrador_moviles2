b38e7604573001a5546f22f294f11f9ef0858494
Camilo0291 <acquintanam@gmail.com>
2024-09-19 18:58:32 -0500

    Se agregaron diferentes cosas

--- app/src/main/java/com/example/gatopardomoviles2/login.java
+++ app/src/main/java/com/example/gatopardomoviles2/login.java
@@ -1,24 +1,41 @@
+
 package com.example.gatopardomoviles2;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main);
+
+        // username = findViewById(R.id.username);
+        // password = findViewById(R.id.password);
+        // loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(login.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(login.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
